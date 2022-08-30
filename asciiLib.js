dd845f55cf39d4bacd1c7ea41cf6a8bb3bedb37b
Philip Durbin <philipdurbin@gmail.com>
2022-08-30 08:29:43 -0400

    add by rating

--- asciiLib.js
+++ asciiLib.js
@@ -27,9 +27,36 @@
     lines = lines.replace(/\n+$/, '');
     return lines;
   }
+  function getByRating(content) {
+    let lines = '';
+    var x = content.split('\r\n');
+    for (var i = 0; i < x.length; i++) {
+      y = x[i].split('\t');
+      x[i] = y;
+    }
+    let byRating = {};
+    x.shift(); // remove header ("Rating")
+    for (const row of x) {
+      let title = row[1];
+      let rating = parseFloat(row[3]) * 10;
+      if (byRating[rating] == undefined) {
+        byRating[rating] = [];
+      }
+      byRating[rating].push(title);
+    }
+    for (const [rating, movies] of Object.entries(byRating)) {
+      let stars = '';
+      for (var i = 0; i < movies.length; i++) {
+        lines += `${rating / 10}\t${movies[i]}\n`;
+      }
+    }
+    lines = lines.replace(/\n+$/, '');
+    return lines;
+  }
 
-  // Export the function to exports.
+  // Export the functions to exports.
   // In node.js this will be the module.exports.
   // In a browser this will be a function in the global object "asciiLib".
   exports.getByYear = getByYear;
+  exports.getByRating = getByRating;
 })(typeof exports === 'undefined' ? (this['asciiLib'] = {}) : exports);
--- cli.js
+++ cli.js
@@ -2,4 +2,14 @@ let fs = require('fs');
 let filename = 'movies.tsv';
 let content = fs.readFileSync(process.cwd() + '/' + filename).toString();
 const utilities = require('./asciiLib');
-console.log(utilities.getByYear(content));
+const myArgs = process.argv.slice(2);
+switch (myArgs[0]) {
+  case 'rating':
+    console.log(utilities.getByRating(content));
+    break;
+  case 'year':
+    console.log(utilities.getByYear(content));
+    break;
+  default:
+    console.log(utilities.getByYear(content));
+}
